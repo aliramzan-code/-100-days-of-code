d38856753d030c909cbb33335e6b42ca7af8d47f
Ali Ramzan <aliramzan.code@gmail.com>
2023-04-21 22:35:24 +0500

    Day 5

--- Day 5/main.py
+++ Day 5/main.py
@@ -3,7 +3,18 @@ print("#Day 5 , Today we are learning about comments and escape sequnces")
 #In python we use # symbol to comment a single line 
 
 # print("Hello python this is comment")
+
+#And python support multi lines comments 
+
+"""
+this is multi line 
+comment in python we use triple qoutes to do this """
+
+
+
 print("now we are going to use escape sequnce characters in python")
 
-#to break the line we use \n and to
+#to break the line we use \n
+print("Hello \n World!")
+
 
